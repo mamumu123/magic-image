564e45884624668d8d518b68800a56098c31d430
lem <11597313+lemC@user.noreply.gitee.com>
2024-06-27 10:43:07 +0800

    chore: rm

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      sharp$: false,
-      'onnxruntime-node$': false,
-    };
-
-    return config;
-  },
 };
 
 export default withNextIntl(nextConfig);
